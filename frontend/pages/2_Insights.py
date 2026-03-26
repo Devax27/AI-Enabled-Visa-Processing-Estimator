1857c91e9c0b2d70c3016d7af5272c31f8470a9b
Devax27 <devangupta27052004@gmail.com>
2026-03-26 16:35:27 +0530

    Insight error resolve

--- frontend/pages/2_Insights.py
+++ frontend/pages/2_Insights.py
@@ -1,9 +1,14 @@
 import streamlit as st
 import pandas as pd
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+DATA_PATH = os.path.join(BASE_DIR, "data", "VisaFile.csv")
+
 
 st.title("📊 Visa Insights")
 
-df = pd.read_csv("../data/VisaFile.csv")
+df = pd.read_csv(DATA_PATH)
+
 
 st.subheader("Top Visa Types")
 st.bar_chart(df["VISA_CLASS"].value_counts().head(10))
